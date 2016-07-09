613ea69c52bb98207c513a53b074ce989c6fc685
Hongwei Zhang <hongwei.zhang@osthus.com>
2016-07-09 20:07:37 +0200

    test Jenkins property file

---
+++ src/main/java/A2Helo.java
@@ -0,0 +1,11 @@
+
+
+import java.util.ResourceBundle;
+
+public class A2Helo {
+	public static void main(String[] args) {
+		ResourceBundle res = ResourceBundle.getBundle("message");
+		System.out.println(res.getString("welcome"));
+		System.out.println(res.getString("name"));
+	}
+}
