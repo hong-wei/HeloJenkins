69cf6fe8ac4b1ecb1993b06a7c7bec2f0659fbd4
Hongwei Zhang <hongwei.zhang@osthus.com>
2016-07-09 19:59:33 +0200

    add three lines

--- src/main/java/helo.java
+++ src/main/java/helo.java
@@ -2,5 +2,9 @@
 public class helo {
 public static void main(String[] args) {
 	System.out.println("helo");
+	System.out.println("helo");
+	System.out.println("helo");
+	System.out.println("helo");
+	System.out.println("helo");
 }
 }
